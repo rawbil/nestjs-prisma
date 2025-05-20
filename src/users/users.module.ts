ae422bdc2d30aa5eb3f2ad30fbfc8c8efbe2fd59
Bil <bildadsimiyu6@gmail.com>
2025-05-20 13:24:05 +0300

    feat(user): add methods in the services and perform create user logic

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-//import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService]
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
     //inject prisma service
-   // constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {}
+
+    createUser(data: Prisma.UserCreateInput) {
+        this.prisma.user.create({data})
+    }
+
+    getUsers() {}
+
+    getUserById() {}
 }
