7a0122c63ba0f2be453078427c12d72ef6e3e05b
Bil <bildadsimiyu6@gmail.com>
2025-05-20 12:19:47 +0300

    feat(DB): connect to database

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-
 @Injectable()
-export class PrismaService extends  PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect()
+      .then(() => console.log('Database connected'))
+      .catch((err) => console.log(err));
+  }
+}
