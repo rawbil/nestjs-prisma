ed094180a792926fba46b3abb9f12c1313cc1ebb
Bil <bildadsimiyu6@gmail.com>
2025-05-20 13:38:54 +0300

    refactor: add valiation pipe to the request body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Prisma } from '@prisma/client';
 import { CreateUserDto } from './dtos/CreateUser.dto';
@@ -8,6 +8,7 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post('create-user') //users/create-user
+    @UsePipes(ValidationPipe)
     createUser(@Body() createUserDto: CreateUserDto) {
         return this.usersService.createUser(createUserDto)
     }
